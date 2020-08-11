0d72d579a8a671cd3bdd2b793d8449ae40b7dedc
MEGBLETO carnel <carnelilordwi@gmail.com>
2020-08-12 03:34:58 +0530

    omplementation of score increase and word change function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,100 @@
-cont word = document.getElementById('word');
-cont text = document.getElementById('text');
-cont scoreEl = document.getElementById('score');
-cont timeEl = document.getElementById('time');
-cont endgameEl = document.getElementById('end-game');
-cont settingsBtn = document.getElementById('setting-btn');
-cont settings = document.getElementById('settings');
-cont settingsForm = document.getElementById('setting-form');
-cont difficultySelect = document.getElementById('difficulty');
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game');
+const settingsBtn = document.getElementById('setting-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('setting-form');
+const difficultySelect = document.getElementById('difficulty');
+
+
+
+
+
+//List of words
+
+const words = [
+
+'sigh',
+'tense',
+'awesome',
+'ball',
+'juice',
+'tomatoes',
+'child',
+'eight',
+'egg',
+'love',
+'stewart',
+'admit',
+'fever',
+'virus'
+];
+
+
+//init word
+
+let randomword;
+
+
+//init score
+
+let score = 0;
+
+
+//init time
+
+let time = 10;
+
+
+//focus text on my input field
+
+text.focus();
+
+
+//genrate radom word from y array
+function getRandomWord(){
+  return words[Math.floor(Math.random()* words.length)];
+}
+
+
+//add random word to the DOM
+
+function addWordToDom(){
+
+randomword = getRandomWord()
+word.innerHTML = randomword;
+scoreEl.innerHTML = score;
+
+}
+
+addWordToDom();
+
+//increase score function
+
+function increaseScore(){
+  score++;
+}
+
+//add an event listeners to my input
+
+
+text.addEventListener('input', (e)=>{
+const insertedtext = e.target.value;
+
+if(insertedtext === randomword){
+  addWordToDom();
+
+  increaseScore();
+
+  
+//empty the field
+
+e.target.value = '';
+}
+  else{
+    console.log('no match');
+  }
+}
+)
